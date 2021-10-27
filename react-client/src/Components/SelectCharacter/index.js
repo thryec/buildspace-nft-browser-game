f3d130169b9a683bacaee9a82bcd3104b80f0061
thryec <thryec@gmail.com>
2021-10-27 15:59:24 +0800

    original version w/o ipfs

---
+++ react-client/src/Components/SelectCharacter/index.js
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { CONTRACT_ADDRESS, transformCharacterData } from "../../constants";
+import myEpicGame from "../../utils/MyEpicGame.json";
+import LoadingIndicator from "../LoadingIndicator";
+import "./SelectCharacter.css";
+
+const SelectCharacter = ({ setCharacterNFT }) => {
+  const [characters, setCharacters] = useState([]);
+  const [gameContract, setGameContract] = useState(null);
+  const [mintingCharacter, setMintingCharacter] = useState(false);
+
+  const mintCharacterNFTAction = async (characterId) => {
+    try {
+      if (gameContract) {
+        setMintingCharacter(true);
+        console.log("Minting character in progress...");
+        const mintTxn = gameContract.mintCharacterNFT(characterId);
+        await mintTxn.wait();
+        console.log("mintTxn: ", mintTxn);
+        setMintingCharacter(false);
+      }
+    } catch (error) {
+      console.log("MintCharacterAction Error:", error);
+      setMintingCharacter(false);
+    }
+  };
+
+  const renderCharacters = characters.map((character, index) => (
+    <div className="character-item" key={character.name}>
+      <div className="name-container">
+        <p>{character.name}</p>
+      </div>
+      <img src={character.imageURI} alt={character.name} />
+      <button
+        type="button"
+        className="character-mint-button"
+        onClick={() => mintCharacterNFTAction(index)}
+      >{`Mint ${character.name}`}</button>
+    </div>
+  ));
+
+  useEffect(() => {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const gameContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        myEpicGame.abi,
+        signer
+      );
+      setGameContract(gameContract);
+    } else {
+      console.log("Ethereum object not found.");
+    }
+  }, []);
+
+  useEffect(() => {
+    const getCharacters = async () => {
+      try {
+        console.log("Getting contract characters to mint");
+
+        const charactersTxn = await gameContract.getAllDefaultCharacters();
+        console.log("charactersTxn: ", charactersTxn);
+
+        const characters = charactersTxn.map((characterData) => {
+          return transformCharacterData(characterData);
+        });
+        setCharacters(characters);
+      } catch (error) {
+        console.log("Something went wrong with fetching characters:", error);
+      }
+    };
+
+    const onCharacterMint = async (sender, tokenId, characterIndex) => {
+      console.log(
+        `CharacterNFTMinted - sender: ${sender} tokenId: ${tokenId.toNumber()} characterIndex: ${characterIndex.toNumber()}`
+      );
+      if (gameContract) {
+        const characterNFT = await gameContract.checkIfUserHasNFT();
+        console.log("Character NFT: ", characterNFT);
+        setCharacterNFT(transformCharacterData(characterNFT));
+      }
+    };
+
+    if (gameContract) {
+      getCharacters();
+      gameContract.on("CharacterNFTMinted", onCharacterMint);
+    }
+    return () => {
+      if (gameContract) {
+        gameContract.off("CharacterNFTMinted", onCharacterMint);
+      }
+    };
+  }, [gameContract]);
+
+  return (
+    <div className="select-character-container">
+      <h2> Mint your hero. Choose wisely. </h2>
+      {characters.length > 0 && (
+        <div className="character-grid">{renderCharacters}</div>
+      )}
+      {mintingCharacter && (
+        <div className="loading">
+          <div className="indicator">
+            <LoadingIndicator />
+            <p>Minting In Progress...</p>
+          </div>
+          <img
+            src="https://media2.giphy.com/media/61tYloUgq1eOk/giphy.gif?cid=ecf05e47dg95zbpabxhmhaksvoy8h526f96k4em0ndvx078s&rid=giphy.gif&ct=g"
+            alt="Minting loading indicator"
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SelectCharacter;
---
+++ react-client/src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = "0x2A1f6bB918bFE3052817235BFA8A9a6cCed10250";
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
